a9fc8fc15c79f1d56b38d43b50d8a662fd58bfe2
ialexca <poupart.a@gmail.com>
2013-07-24 14:47:25 -0400

    enlever les fichiers deplaces a la racine du projet

--- source/weha/EditHistoryAnalyzer.i18n.php
+++
@@ -1,9 +0,0 @@
-<?php
-$messages = array();
- 
-/* *** English *** */
-$messages['en'] = array( 
-	'weha' => 'Edit Analysis',
-	'weha_desc' => "Provide wiki-syntax aware analysis of edit history",
-	'weha_tabname' => 'edit analysis'
-);
