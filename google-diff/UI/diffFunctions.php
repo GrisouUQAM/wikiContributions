a70775d1cf6aca79a87b7766eefb27b17b6559a2
nikixiaoyou <Administrator@ThinkPadLove.(none)>
2013-08-01 15:37:31 -0400

    google-diff en php

--- google-diff/UI/diffFunctions.php
+++ google-diff/UI/diffFunctions.php
@@ -1,55 +1,48 @@
 ﻿<?php
+require_once('diff_match_patch.php');
+
+function dmp(){
+	static $dmp = null;
+	if(!$dmp){
+		$dmp = new diff_match_patch();
+	}
+	return $dmp;
+}
+
 function getDiff ($text1, $text2) {
-        //Params
-        $separator = chr(30); //LF pour separer les deux texte (norme Linux/Unix)
-        $filename = 'textcomp.tmp';
-        $mode = 'w'; // Creer/ecraser le fichier existant
-        
-        //Ecriture dans le fichier temporaire
-        $file = fopen($filename, $mode);
-        fwrite($file, $text1);
-        fwrite($file, $separator);
-        fwrite($file, $text2);
-        fclose($file);
-        
-        //tdiff avec le fichier temporaire en argument
-	$stringCommand = '"../bin/tdiff.exe" ';
-        $stringCommand .= '"' . $filename . '"';
-        $res = Array();
-        exec($stringCommand, $res);
-        
+	$res = dmp()->diff_main($text1, $text2, false);	
+	$newDmp = new diff_match_patch();
+	$newDmp->diff_cleanupSemantic($res);
 	return $res;
 }
 
 
 function prettyHtml($diffs) {
-	$html = "";
+	$equalCount = 0;
+	$deleteCount = 0;
+	$insertCount = 0;
+	$sentenceMatchingRate = 0.00;
+	$results = dmp()->diff_prettyHtml($diffs);
 	for ($x = 0; $x < sizeof($diffs); $x++) {
-		$diffArray = explode('Diff', $diffs[$x]);
-		for ($y = 0; $y < sizeof($diffArray); $y++) {
-			$newArray = explode(',"', $diffArray[$y]);
-			$op = $newArray[0];    // Operation (insert, delete, equal)
-			if (sizeof($newArray) > 1) {
-				$text = explode('")',$newArray[1]);  // Text of change.
-				$data = $text[0];
-			} else {
-				$data = "";
-			}			
-			switch ($op) {
-				case "(INSERT":
-				$html .= '<ins style="background:#e6ffe6;">'."INSERT".'</ins>';
-				break;
-				case "(DELETE":
-				$html .= '<del style="background:#ffe6e6;">'.'DELETE'.'</del>';
-				break;
-				//case "(EQUAL":
-				//$html .= '<span>'.$data.'</span>';
-				//break;
-			}
+		switch ($diffs[$x][0]){
+			case 0:
+			$equalCount++;
+			break;
+			case 1:
+			$insertCount++;
+			break;
+			case -1:
+			$deleteCount++;
+			break;
 		}
 	}
-	
-	return $html;
+	$sentenceMatchingRate = $equalCount/sizeof($diffs);
+	$results .= "Number of words deleted : ".$deleteCount."";
+	$results .= ("<br/>");
+	$results .= "Number of words inserted : ".$insertCount."";
+	$results .= ("<br/>");
+	$results .= "sentence matching rate : ".$sentenceMatchingRate."";
+	return $results;
 }
 
 
