c653c512c66f7f0416f9bd31ff0f136a7c977cd1
lunfel <mathieu.tanguay871@gmail.com>
2013-09-24 11:41:52 -0400

    Réparation du gif de loading

--- src/script/contributions-script.js
+++ src/script/contributions-script.js
@@ -3,9 +3,11 @@ function sendForm() {
     var sent = false;
     var us = $("#user").val();
     var ur = $("#url").val();
-
-
+    
+    $('#queryloading').append('<img class="isloading" src="images/miniloading.gif" />');
+    
     $.get("contributions.php", {user: us, wiki: ur}, function(data) {
+	$('.isloading').remove();
 	$("#result").html(data);
 	var values = [];
 	var labels = [];
